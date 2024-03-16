7eac0fe21b6e22e8f2e25c48ab2fbc14fe697e4c
jaewoo23 <thomas062369@gmail.com>
2024-03-16 18:53:28 +0900

    hw1

--- hw1.py
+++ hw1.py
@@ -6,7 +6,7 @@ def get_circle_area(radius):
     a = float(radius) * float(radius) * 3.14
     return a
 
-prompt = input("넓이를 구하고자 하는 원의 반지름은?")
+prompt = "넓이를 구하고자 하는 원의 반지름은?"
 result=get_radius(prompt)
 area = get_circle_area(result)
 print("반지름",result, "인 원의 넓이 = 3.14 x ", result, "x", result, "=", area)
